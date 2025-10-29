d19f7da2cba47ffe259b6cfc24d40c402979fdfb
arabi kabir <mail2arabi.com>
2025-10-29 17:25:28 +0600

    docker issue fix

--- aiservice/src/main/java/com/fitness/aiservice/AiServiceApplication.java
+++ aiservice/src/main/java/com/fitness/aiservice/AiServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
+@EnableKafka
 public class AiServiceApplication {
 
 	public static void main(String[] args) {
